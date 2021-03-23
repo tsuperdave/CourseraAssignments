35498324b5223beddb92743890d68047a99ffc36
thesuperdave <davetad903@gmail.com>
2021-03-23 17:16:45 -0500

    reworked check_brackets code

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.java
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.java
@@ -30,18 +30,39 @@ class check_brackets {
         String text = reader.readLine();
 
         Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        int errorPos = 0;
+        
         for (int position = 0; position < text.length(); ++position) {
             char next = text.charAt(position);
 
             if (next == '(' || next == '[' || next == '{') {
                 // Process opening bracket, write your code here
+            	Bracket bracket = new Bracket(next, position + 1);
+            	opening_brackets_stack.push(bracket);
             }
 
             if (next == ')' || next == ']' || next == '}') {
                 // Process closing bracket, write your code here
+            	if(opening_brackets_stack.empty()) {
+            		errorPos = position + 1;
+            		break;
+            	}
+            	Bracket top = opening_brackets_stack.pop();
+            	if(!top.Match(next)) {
+            		errorPos = position + 1;
+            				break;
+            	}
             }
         }
-
         // Printing answer, write your code here
+        if(errorPos == 0 && opening_brackets_stack.empty()) System.out.println("Success");
+        else {
+        	if(errorPos == 0) {
+        		while(opening_brackets_stack.size() > 1)
+        			opening_brackets_stack.pop();
+        		errorPos = opening_brackets_stack.peek().position;
+        	}
+        	System.out.println(errorPos);
+        }
     }
 }
